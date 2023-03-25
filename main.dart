7ff01af7a56ec002e705dc55fa4607f287655637
Mohamed Farag <109981210+mohamedfarag22@users.noreply.github.com>
2023-03-25 21:37:09 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("First Application"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(
+          "Mohamed Ahmed Mahmoud Farag",
+        ),
+      ),
+    ),
+  ));
+}
